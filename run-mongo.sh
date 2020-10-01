ecade0eb79c9876d274970cf00736722092f433e
Frank Tan <franktanah@gmail.com>
2020-10-01 15:33:31 +0800

    Update run-mongo.sh

--- run-mongo.sh
+++ run-mongo.sh
@@ -1,7 +1,3 @@
-echo Cleanup...
-docker kill mymongo 2>/dev/null
-docker rm mymongo 2>/dev/null
-
 echo Starting MongoDB
 docker run --name mymongo -d mongo
 echo Hang on for a moment...
@@ -9,4 +5,3 @@ sleep 10
 echo Starting Mongo Shell
 docker exec -it mymongo mongo
 
-echo "MongoDB Shell stopped. Did you press Ctrl-C maybe? Anyway, simply run the $0 script again like previously."
